e56dbf941c4a48904c5b19d48ca26f5cc59e0392
Viachaslau Fedartsou <viachaslaufedartsou@gmail.com>
2023-09-02 11:41:18 +0300

    feat: add dark theme

---
+++ js/dark-theme.js
@@ -0,0 +1,29 @@
+const html = document.documentElement;
+
+const buttonThemeToggle = document.getElementById('button-toggle');
+const iconThemeToggle = document.getElementById('icon-toggle');
+
+const toogleTheme = () => {
+  buttonThemeToggle.addEventListener('click', (event) => {
+    event.preventDefault();  
+
+    localStorage.getItem('theme') === 'dark' ? localStorage.removeItem('theme') : localStorage.setItem('theme', 'dark');  
+
+    addClass();
+  });
+
+  const addClass = () => {  
+    if (localStorage.getItem('theme') === 'dark') {
+      html.classList.add('dark');
+      iconThemeToggle.textContent = 'light_mode'; 
+    } else {
+      html.classList.remove('dark');
+      iconThemeToggle.textContent = 'nightlight_round';
+    }       
+  }
+
+  addClass();
+}
+
+export default toogleTheme;
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import toogleBurger from './burger-menu.js';
 import changeLanguage from './languages.js';
+import toogleTheme from './dark-theme.js';
 
 toogleBurger();
 changeLanguage();
+toogleTheme();
